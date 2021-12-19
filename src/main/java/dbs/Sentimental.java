b5348a66de13395c69b86a25092f0d9976cd9404
MandarinenMinister <michael.duell@student.uni-augsburg.de>
2021-12-19 16:38:19 +0100

    Kinda working

--- src/main/java/dbs/Sentimental.java
+++ src/main/java/dbs/Sentimental.java
@@ -12,29 +12,26 @@ import org.apache.hadoop.io.*;
 import java.nio.file.*;
 import org.apache.commons.*;
 import java.nio.file.Files;
+import java.time.Instant;
 
 
 public class Sentimental {
 	static int filecount = 0;
-	public static Path split_Dir = null;											//Input directory of the original large Json Files
+	public static Path split_Dir = Paths.get("D:\\DBSProjekt\\Raw_Data");											//Input directory of the original large Json Files
 	public static Path input_Dir = Paths.get("D:\\DBSProjekt\\Datein");						//Input directory of the Split up Json Files
 	public static Path result_Dir = Paths.get("D:\\DBSProjekt\\Ergebnis");					//Output directory of the result file containing only the text from negative sentiment tweets without metadata
 	public static Path temp_Dir_1 = Paths.get("D:\\DBSProjekt\\zu_pruefen");					//A temporary directory
 	public static Path temp_Dir_2 = Paths.get("D:\\DBSProjekt\\zu_pruefen_neu\\");			//Directory for the filtered files before the sentiment analysis 
-	public static Path filter_Dir = Paths.get("D:\\DBSProjekt\\FilterListen\\Homophobic.csv");	//The Input directory and file name of the list of words used as a filter
+	public static Path filter_Dir = Paths.get("D:\\DBSProjekt\\FilterListen\\Ethnic.csv");	//The Input directory and file name of the list of words used as a filter
 	public static Path temp_Dir_3 = Paths.get("D:\\DBSProjekt\\Output");							//Temporary output path for the sentiment analysis, containing still both positive and negative tweets
 	public static Path negatives = Paths.get("D:\\DBSProjekt\\Negatives\\");			//Finaler outputpfad in dem die Negativen Sentiment Dateien gespeichert werden
 	public static String neg = "sentiment: Negative):";		
 	public static String veryneg = "sentiment: Very Negative):";	
 	public static Scanner scan;
+	
 	 public static void main(String[] args) {
 		filecount++;
-		boolean error = false;
-		try {
-			Files.createDirectories(Paths.get(temp_Dir_1.toString() + "\\" + Integer.toString(filecount)));
-		} catch (IOException e8) {
-			e8.printStackTrace();
-		}
+		long unixstart = Instant.now().getEpochSecond();
 		StanfordCoreNLP.OutputFormat.valueOf("JSON");									//settings für meine lokale maschine
 	    System.setProperty("hadoop.home.dir", "C:\\hadoop-3.3.1");
 	    SparkConf sparkConf = new SparkConf().setAppName("Hate_Speech_Filter");
@@ -63,7 +60,7 @@ public class Sentimental {
 	    int i = 1;
 	    for(Path input_path : inputs)
 	    {	
-		    Dataset<Row> zu_pruefen = sparkSession.read().option("inferSchema", true).json(input_path.toString() + "\\" + "Datei" + Integer.toString(i)+ ".json"); //Pfad und name der einzulesenden Dateien hier "Datei(i).json"
+		    Dataset<Row> zu_pruefen = sparkSession.read().option("inferSchema", true).json(input_path.toString()); //Pfad und name der einzulesenden Dateien hier "Datei(i).json"
 		    zu_pruefen.createOrReplaceTempView("zu_pruefen_view");
 		    filterdatei = sparkSession.sql("Select text from zu_pruefen_view , filter_liste WHERE zu_pruefen_view.text LIKE ('%' || ' ' || filter_liste.term || ' ' || '%') ");  //pruefen auf enthalten der Filterliste
 		    filterdatei.write().json(temp_Dir_1.toString() + "\\" + Integer.toString(i));			//Output der gefilterten Datei, auf welche die sentimentanalyse ausgeführt wird
@@ -82,6 +79,11 @@ public class Sentimental {
 			} catch (IOException e6) {
 				e6.printStackTrace();
 				System.exit(9);
+			}
+			try {
+				Files.createDirectories(Paths.get(temp_Dir_1.toString() + "\\" + Integer.toString(filecount)));
+			} catch (IOException e8) {
+				e8.printStackTrace();
 			}
 	 	    Object[] b_array_temp = b_stream.toArray();
 	 	    b_stream.close();
@@ -106,7 +108,7 @@ public class Sentimental {
 		    	else if(d.toString().contains(".json"))
 		    	{
 		    		try {
-						Files.move(d, Paths.get(temp_Dir_1.toString() + "\\" + Integer.toString(filecount) + "\\Datei_" + Integer.toString(i) + "_" + Integer.toString(y) + ".json"),StandardCopyOption.REPLACE_EXISTING);
+						Files.move(d, Paths.get(temp_Dir_2.toString() + "\\" + Integer.toString(filecount) + "\\Datei_" + Integer.toString(i) + "_" + Integer.toString(y) + ".json"),StandardCopyOption.REPLACE_EXISTING);
 					} catch (IOException e1) {
 						e1.printStackTrace();
 						System.exit(9);
@@ -117,7 +119,7 @@ public class Sentimental {
 		    Properties props = new Properties();
 		    props.setProperty("annotators", "tokenize, ssplit, pos, parse,sentiment");
 		    StanfordCoreNLP Pipeline = new StanfordCoreNLP(props);
-		    Path e = Paths.get(temp_Dir_1.toString() + "\\" + Integer.toString(i) + "\\");
+		    Path e = Paths.get(temp_Dir_2.toString() + "\\" + Integer.toString(i) + "\\");
 	    	Stream<Path> e_stream = null;
 			try {
 				e_stream = Files.walk(e);
@@ -155,8 +157,7 @@ public class Sentimental {
 
 	    	Path outputs = Paths.get(temp_Dir_3.toString());  // Filtern der Sentimentanalys auf negative und sehr negative ergebnisse
 	    	int z = 0;
-	    	if(!error) {
-		    	Stream<Path> outputs_stream;
+		    	Stream<Path> outputs_stream = null;
 				try {
 					outputs_stream = Files.walk(outputs);
 				} catch (IOException e4) {
@@ -169,39 +170,29 @@ public class Sentimental {
 		    	for(Path g : outputs_array) 
 		    	{
 		    		String temp_string;
-		    		try {
-						 scan = new Scanner(g);
-						 while(scan.hasNextLine()) 
+		    		scan = new Scanner(g.toString());
+					 while(scan.hasNextLine()) 
+					 {
+						 temp_string = scan.nextLine();
+						 if(temp_string.contains(neg) || temp_string.contains(veryneg)) 
 						 {
-							 temp_string = scan.nextLine();
-							 if(temp_string.contains(neg) || temp_string.contains(veryneg)) 
+							 scan.close();
+							 try 
 							 {
-								 scan.close();
-								 try 
-								 {
-									 Files.move(g, Paths.get(negatives.toString() + "\\File_" + Integer.toString(i) + "_" + Integer.toString(z)));
-								 } 
-								 catch (IOException e2) 
-								 {
-									e2.printStackTrace();
-									System.exit(9);
-								 }
-								 break;
+								 Files.move(g, Paths.get(negatives.toString() + "\\File_" + Integer.toString(i) + "_" + Integer.toString(z)));
+							 } 
+							 catch (IOException e2) 
+							 {
+								e2.printStackTrace();
+								System.exit(9);
 							 }
+							 break;
 						 }
-						scan.close();
-						}
-		    			catch (FileNotFoundException e3) 
-			    			{
-								e3.printStackTrace();
-							} catch (IOException e1) {
-							e1.printStackTrace();
-							System.exit(9);
-						}
+					 }
+					scan.close();
 		    			z++;
-		    	}
-	    	}
-	    	
+		    	} 
+		    	i++;
 	    	}
     	Stream<Path> negatives_stream = null;
 		try {
@@ -216,16 +207,7 @@ public class Sentimental {
 	    for(Path h : negatives_array)
 	    	{
 		    	String temp_string;
-		    		try {
-						scan = new Scanner(h);
-					} catch (FileNotFoundException e4) {
-						System.out.print("Fehler beim einscannen der Negativen sentiment dateien");
-						e4.printStackTrace();
-						System.exit(9);
-					} catch (IOException e) {
-						e.printStackTrace();
-						System.exit(9);
-					}
+		    		scan = new Scanner(h.toString());
 		    		
 					 while(scan.hasNextLine()) {
 						 temp_string = scan.nextLine();
@@ -250,5 +232,61 @@ public class Sentimental {
 			e6.printStackTrace();
 			System.exit(9);
 		}
+	    runtime(unixstart);
 	 }
+
+
+
+	 public static void runtime(long unixstart) {
+
+		long unixend = Instant.now().getEpochSecond(); // Endzeit
+		int x = (int) (unixend - unixstart) / 60;
+		System.out.println("Stunden: " + (x / 60) + " Minuten: " + (x % 60) + " Sekunden: " + ((unixend - unixstart) % 60));
+	}
+
+	 public static void split() {
+		Stream <Path> split_stream = null;
+		try {
+			split_stream = Files.walk(split_Dir);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(8);
+		}
+		Object[] split_array_temp = split_stream.toArray();
+	 	split_stream.close();
+	 	Path[] split_array = Arrays.copyOf(split_array_temp, split_array_temp.length, Path[].class);
+	 	int x = 1;
+	 	FileWriter splitter = null;
+	 	for(Path tweets : split_array)
+	 	{
+	 		Scanner tweetscanner = new Scanner(tweets.toString());
+	 		while(tweetscanner.hasNextLine()) {
+	 			try {
+					splitter = new FileWriter(input_Dir.toString() + "\\" + "Datei" + Integer.toString(x) + ".json");
+				} catch (IOException e) {
+					e.printStackTrace();
+					System.exit(x);
+				}
+	 			for(int i = 1; i <= 10000;i++)
+	 			{
+	 				try {
+						splitter.write(tweetscanner.nextLine());
+					} catch (IOException e) {
+						e.printStackTrace();
+						System.exit(x);
+					}
+	 			}
+	 			try {
+					splitter.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+					System.exit(x);
+				}
+	 			x++;
+	 		}
+	 	}
+	 }
+
+
 }
+
