2d1c30318a6feb7c25dd232acd3ac529fe1cbe0c
Dani <95964411+dvarlan@users.noreply.github.com>
2022-03-05 22:30:09 +0100

    Update Sentimental.java

--- src/main/java/dbs/Sentimental.java
+++ src/main/java/dbs/Sentimental.java
@@ -602,7 +602,7 @@ public class Sentimental {
 			 System.out.println("Debug3");
 			 System.exit(9);
 		}
-		java.util.list<FileWriter> writers = new ArrayList<FileWriter>();
+		java.util.List<FileWriter> writers = new ArrayList<FileWriter>();
 		
 		for (int i = 0; i <= 46; i++) {
 		    FileWriter w;
