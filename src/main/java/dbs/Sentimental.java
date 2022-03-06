14eae104537bd39a1687dbf1e9c7788693b6a040
Michi <45259816+MandarinenMinister@users.noreply.github.com>
2022-03-06 07:50:30 +0100

    java.util.List

--- src/main/java/dbs/Sentimental.java
+++ src/main/java/dbs/Sentimental.java
@@ -144,7 +144,7 @@ public class Sentimental {
 		}
 		Object[] cloudarray = cloud_stream.toArray();
 	    Path[] cloudpaths = Arrays.copyOf(cloudarray, cloudarray.length, Path[].class);
-	    List<WordFrequency> frequencyList = null;
+	    java.util.List<WordFrequency> frequencyList = null;
 		FrequencyFileLoader loader = new FrequencyFileLoader();
 		boolean once = true;
 	    for(Path cloud_path : cloudpaths)
