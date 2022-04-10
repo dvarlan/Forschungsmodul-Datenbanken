11f68ffbde214b3d5e997a46d5cf4e01f00b91ac
Michi <45259816+MandarinenMinister@users.noreply.github.com>
2022-04-10 17:54:13 +0200

    fix

--- src/main/java/dbs/Sentimental.java
+++ src/main/java/dbs/Sentimental.java
@@ -133,7 +133,7 @@ public class Sentimental {
 					 	if(tweet_counter[i] != 0)
 						 {
 							 //percent = hate_tweets[i]/tweet_counter[i];
-							 int percent = (int)(hate_tweets[i] * 100.0 /tweet_counter[i] + 0.5);
+							 percent = (int)(hate_tweets[i] * 100.0 /tweet_counter[i] + 0.5);
 
 						 }
 						 else
